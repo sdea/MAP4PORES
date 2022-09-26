84b5f6fece16661745b13a1d722e1a5b2d3e8f55
Salvatore De Angelis <salvatore.deangelis89@gmail.com>
2022-09-26 20:04:26 +0200

    Added phase fractions and representative volumes

---
+++ metrics/compute_phase_fraction_profile.m
@@ -0,0 +1,6 @@
+function compute_phase_fraction_profile(im, label, direction)
+% TODO: - to add documentation
+
+% Assert if 2D or 3D 
+
+end
---
+++ metrics/representative_volume.m
@@ -0,0 +1,5 @@
+function [sizes, value] = representative_volume(im, num_points)
+% TODO: - to add documentation
+
+% Assert if 2D or 3D
+end
