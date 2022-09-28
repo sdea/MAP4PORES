74d1e92f91e8b5e28c87ddbfe4f2f557218f2ef6
Salvatore De Angelis <salvatore.deangelis89@gmail.com>
2022-09-28 11:05:54 +0200

    Inclued support for 3D

--- filters/label_connected.m
+++ filters/label_connected.m
@@ -1,22 +1,31 @@
 function im_out = label_connected(im, faces)
-% This function only works in 2D for now
-% Make seamingless work in 3D 
-% Also, introduce the mask parameter for arbitrary edge shape connectivity
+% TODO: include documentation
+
+im_dims = length(size(im));
+is3D = im_dims == 3;
 
 switch nargin
     case 0
         disp("Not enough inputs arguments")
         return
     case 1
-        faces = [1, 1, 1, 1];
+        if is3D 
+            faces = [1, 1, 1, 1, 1, 1];
+        else
+            faces = [1, 1, 1, 1];
+        end
 end
 
-im_label = bwlabel(im);
+if is3D
+    im_label = bwlabel(im);
+else  
+    im_label = bwlabeln(im);
+end
+
 num_labels = max(im_label(:));
 
-% Make 
 im_borders = create_borders(im, faces);
-im_connected = zeros(size(im));
+im_out = zeros(size(im));
 
 for it=1:num_labels
 
@@ -24,13 +33,11 @@ for it=1:num_labels
     intersect = tmp_mask & im_borders;
 
     if isempty(find(intersect == true, 1))
-        im_connected(tmp_mask) = 1;
+        im_out(tmp_mask) = 1;
     else
-        im_connected(tmp_mask) = 2;
+        im_out(tmp_mask) = 2;
     end
 
 end
-
-im_out = im_connected;
 
 end
