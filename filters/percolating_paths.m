7dc011c509d16fd5aa41f98b4b72be1a27fac1af
Salvatore De Angelis <salvatore.deangelis89@gmail.com>
2022-09-29 13:59:27 +0200

    Added the function for percolating paths

---
+++ filters/percolating_paths.m
@@ -0,0 +1,10 @@
+function im_out = percolating_path(im, faces)
+% TODO: Add documentaiton 
+% This function results in an image presenting only the percolating paths 
+% from source and destination 
+
+
+
+
+
+end
